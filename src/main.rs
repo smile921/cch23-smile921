516ca4336c4ace30c53e2288455e6064465bb703
LanYi <ruby2010ruby@163.com>
2023-12-03 13:13:32 +0800

    init days

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
-use axum::{routing::get, Router};
+use axum::{routing::get};
 use tower_http::services::ServeDir;
+use tower_http::trace;
+
+mod days;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello_world))
-        .nest_service("/assets", ServeDir::new("assets"));
 
-    Ok(router.into())
+struct CCHService {
+	router: axum::Router,
 }
+
+#[allow(clippy::unused_async)]
+#[shuttle_runtime::main]
+async fn init() -> Result<CCHService, shuttle_runtime::Error> {
+	let router_days = days::router();
+    let router = router_days
+        .route("/hello", get(hello_world))
+        .nest_service("/assets", ServeDir::new("assets"))
+		.layer(trace::TraceLayer::new_for_http()
+			.make_span_with(trace::DefaultMakeSpan::new()
+				.level(tracing::Level::INFO))
+			.on_response(trace::DefaultOnResponse::new()
+				.level(tracing::Level::INFO)));
+
+	Ok(CCHService { router })
+}
+
+
+#[shuttle_runtime::async_trait]
+impl shuttle_runtime::Service for CCHService {
+	async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
+		let router = self.router;
+		let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+		tokio::select!(() = async {axum::serve(listener, router).await.unwrap()} => {});
+
+		Ok(())
+	}
+} 
+
+    // let router = Router::new()
+    //     .route("/", get(hello_world))
+    //     .nest_service("/assets", ServeDir::new("assets"));
