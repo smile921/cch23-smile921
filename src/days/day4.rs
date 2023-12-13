23d7210d276b7158aeefa969396aa568885ef0c4
LanYi <ruby2010ruby@163.com>
2023-12-13 22:51:56 +0800

    day 4 task 1

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,3 +1,25 @@
+use axum::{Json, response::IntoResponse};
+
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[derive(Debug,Clone,Deserialize)]
+pub struct Reindeer {
+    name: String,
+    strength: i32,
+}
+
 pub fn router() -> axum::Router {
-    axum::Router::new().route("/4", axum::routing::get(axum::http::StatusCode::OK))
+    axum::Router::new().route("/4/strength", axum::routing::post(day_four))
+}
+
+
+async fn day_four(Json(payload): Json<Vec<Reindeer>>) -> impl IntoResponse {
+     
+    let sum = payload.into_iter().map(|reinder| reinder.strength).reduce(|a,b| a+b ).unwrap_or_default();
+
+    ( axum::http::StatusCode::OK,
+        sum.to_string() ,
+    ).into_response()
+
 }
