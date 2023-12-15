bac0b2c6375f523c5752b02f8f7c8c1df69b38b2
LanYi <ruby2010ruby@163.com>
2023-12-15 23:36:44 +0800

    todo day6 task one

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,20 @@
-pub fn router() -> axum::Router {
-    axum::Router::new().route("/6", axum::routing::get(axum::http::StatusCode::OK))
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing;
+use axum::{Json, Router};
+use serde::Serialize;
+#[derive(Debug, Serialize)]
+pub struct ResultRet {
+    elf: isize,
+}
+
+pub fn router() -> Router {
+    Router::new().route("/6", routing::post(task_one))
+}
+
+pub async fn task_one( _payload: String ) -> impl IntoResponse {
+    
+    let ret = ResultRet { elf: 1 };
+
+    (StatusCode::OK, serde_json::to_string(&ret).unwrap()).into_response()
 }
