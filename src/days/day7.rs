4f2ee677ca4e15480f64e7ddd0a38370068723df
LanYi <ruby2010ruby@163.com>
2024-01-07 10:21:08 +0800

    first try

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,3 +1,19 @@
+use axum::{http::{HeaderMap, header::COOKIE}, response::IntoResponse, Json};
+
 pub fn router() -> axum::Router {
-    axum::Router::new().route("/7", axum::routing::get(axum::http::StatusCode::OK))
+    axum::Router::new().route("/7/decode", axum::routing::get(get_cookie_recipe))
+}
+
+async fn get_cookie_recipe(headers:HeaderMap)-> impl IntoResponse {
+    let key= "recipe";
+    let cookies= headers.get(COOKIE);
+    let cookie_str = match cookies {
+        None => "",
+        Some(cookie) => {
+            cookies
+                .and_then(|s| s.to_str().ok())
+                .unwrap_or("")
+        }
+    }; 
+    Json(cookie_str).into_response()
 }
