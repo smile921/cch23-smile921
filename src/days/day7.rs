b0cf3b2c92b2bd63cf3048493d6845962beb66c3
LanYi <ruby2010ruby@163.com>
2024-01-07 11:21:49 +0800

    task 1 of day 7

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,10 +1,14 @@
 use axum::{http::{HeaderMap, header::COOKIE}, response::IntoResponse, Json};
+use axum_extra::{TypedHeader, headers::Cookie};
+use base64::{engine::general_purpose, Engine};
+use serde_json::Value;
 
 pub fn router() -> axum::Router {
     axum::Router::new().route("/7/decode", axum::routing::get(get_cookie_recipe))
 }
 
-async fn get_cookie_recipe(headers:HeaderMap)-> impl IntoResponse {
+#[allow(unused)]
+async fn get_cookie_recipe_v01(headers:HeaderMap)-> impl IntoResponse {
     let key= "recipe";
     let cookies= headers.get(COOKIE);
     let cookie_str = match cookies {
@@ -16,4 +20,18 @@ async fn get_cookie_recipe(headers:HeaderMap)-> impl IntoResponse {
         }
     }; 
     Json(cookie_str).into_response()
+}
+
+async fn get_cookie_recipe(cookie: TypedHeader<Cookie>) -> impl IntoResponse {
+
+    let raw_recipe= cookie.get("recipe").unwrap_or_default();
+    println!("recipe {:?}",&raw_recipe);
+    let bytes = general_purpose::STANDARD.decode(raw_recipe).unwrap();
+    let json_recipe = std::str::from_utf8(&bytes).unwrap();
+    println!("decoded {:?}",json_recipe);
+    let recipe:Value = serde_json::from_str(json_recipe).unwrap();
+    Json( recipe
+        )
+        .into_response()
+
 }
